1c6fb21dc18e5fe33034059c1fb4e6fa94b8c95f
Student <Caren-Chepngetich>
2024-03-07 17:19:18 +0300

    added files

---
+++ index.js
@@ -0,0 +1,61 @@
+// Extracting the last four characters in "extravaganza"
+
+
+let a = "extravaganza"
+
+console . log({character:a[9]});
+console . log({character:a[10]});
+console . log({character:a[11]});
+console . log({character:a[12]});
+
+
+
+// Inserting "eat" to "The quick fox jumped over the lazy dog"
+
+const food = "The quick fox jumped over the lazy dog";
+const insertString = "eat"
+const secondfood = food.substring(0,4) + insertString + food.substring(4);
+console.log(secondfood);
+
+
+//Count how many times "the" appears in "The quick brown fox jumps over the lazy dog"
+
+
+const story= "The quick brown fox jumps over the lazy dog";
+console . log((string4.match(/the/g)).length);
+console . log((String4.match(/brown/g)).length);
+
+
+
+//Find "are" in "The pupils are reading in the library"
+
+const string1 = "The pupils are reading in the library"
+const string2 = "The child was sitting on the table before it fell"
+console.log({index:string2.indexOf('sitting')})
+
+
+
+//Converting
+
+Uppercase : "Wonderful"
+
+let word = "wonderful"
+console .log (word.toUppercase);
+
+
+Lowercase :"amazing", "UndERneath"
+
+let word1 = "amazing";
+console .log(word1.toLowerCase);
+
+
+let word2 = "UndERneath";
+console .log(word2.toLowerCase());
+
+
+
+
+//Title case : "A wonderful world"
+
+
+
